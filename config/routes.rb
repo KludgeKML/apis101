ec7c085144ecbc21aee2fae10e075bb4f125d896
K M Lawrence <keith@kludge.co.uk>
2023-11-01 09:49:59 +0000

    Remove roles

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :groups
-      resources :roles
       resources :users
     end
 
     namespace :v2 do
       resources :groups
-      resources :roles
       resources :users
 
       get "/", to: "root#index"
