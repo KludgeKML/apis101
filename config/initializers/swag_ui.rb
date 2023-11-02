06501d72ceafd2f3d26d758b7d378448d98bab7a
K M Lawrence <keith@kludge.co.uk>
2023-11-02 10:06:15 +0000

    Update to add v2 to openapi

--- config/initializers/swag_ui.rb
+++ config/initializers/swag_ui.rb
@@ -1,3 +1,4 @@
 Rswag::Ui.configure do |c|
-  c.swagger_endpoint '/apis101_openapi3.json', 'APIs101 ReSTful v1 Docs'
+  c.swagger_endpoint '/apis101_v1_openapi3.json', 'APIs101 ReSTful v1 Docs'
+  c.swagger_endpoint '/apis101_v2_openapi3.json', 'APIs101 ReSTful v2 Docs'
 end
